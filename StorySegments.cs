fb21bd8baea12ee0ec7270f8f8fcc5ec1e7efd21
SammyJoe Macri <124796253+SammyJoe01@users.noreply.github.com>
2023-05-18 12:39:24 +0100

    Scripts

---
+++ StorySegments.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Story Segment")]
+
+public class StorySegments : ScriptableObject
+{
+    [TextArea(25,14)] public string storyText;
+    public StorySegments[] nextStorySegments;
+}
